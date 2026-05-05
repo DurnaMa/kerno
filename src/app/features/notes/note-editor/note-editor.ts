6aabe69ea0507bd6c95f6765f4ea08b2eb186a75
mdurna <durna.m@outlook.de>
2026-05-05 16:32:35 +0200

    feat: Implement note editor component and integrate into note list.

--- src/app/features/notes/note-editor/note-editor.ts
+++ src/app/features/notes/note-editor/note-editor.ts
@@ -1,17 +1,34 @@
 import { Component } from '@angular/core';
 import { NoteService } from '../note';
+import { FormsModule} from '@angular/forms';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-note-editor',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './note-editor.html',
   styleUrl: './note-editor.css',
 })
+
 export class NoteEditorComponent {
+  @Output() noteSaved = new EventEmitter();
   title: string = '';
   content: string = '';
   saving: boolean = false;
 
   constructor(private noteService: NoteService) {}
 
+  async save() {
+    if (!this.title) return;
+    this.saving = true;
+    try {
+      await this.noteService.createNote({ title: this.title, content: this.content });
+      this.title = '';
+      this.content = '';
+    } catch (error) {
+      console.error('Fehler bein Speichern der Notiz: ', error);
+    } finally {
+      this.saving = false;
+    }
+  }
 }
--- src/app/features/notes/note-list/note-list.ts
+++ src/app/features/notes/note-list/note-list.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NoteService, Note } from '../note';
+import { NoteEditorComponent} from '../note-editor/note-editor';
 
 @Component({
   selector: 'app-note-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NoteEditorComponent],
   templateUrl: './note-list.html',
 })
 export class NoteListComponent implements OnInit {
